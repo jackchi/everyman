682437d0405da5594656ead4bd31648895a09f8a
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2013-04-14 01:06:22 -0700

    tracked down bizarre issue with incomplete mongo queries... saved clinics now get markers placed properly

--- client/instahood.js
+++ client/instahood.js
@@ -2,46 +2,16 @@ Meteor.startup(function(){
 
   clinics = new Meteor.Collection("clinics");
   users = new Meteor.Collection("user");
- geocoder = new google.maps.Geocoder();
- Locations = new Meteor.Collection("locations");
-
-  function geoCode(address){
-        geocoder.geocode({'address':address},function(results,status){
-        console.log(results);
-        results = results[0];
-        if(status == google.maps.GeocoderStatus.OK){
-        // try to avoid recreating map.. but only function that seems to work for now...
-              createMap(results.geometry.location);
-            console.log('ok');
-            console.log(results.geometry);
-            console.log(results.geometry.location.jb);
-            
-            placeNavMarker(results.geometry.location);
-            // this just doesn't want to place...
-            console.log(results.geometry.location.kb);
-
-        }else{
-            console.log('prob');
-        }
-        })
-  
-  }
-/*
-
-
-    user settings:
-    
-    
-    {
-    
-        _id : <mongoid>,
-        email : <string>,
-        clinics : [ <mongoid> , <mongoid> , ..],
-        phone_number : 1291231232
-        
-    }
-
-*/
+  geocoder = new google.maps.Geocoder();
+  Locations = new Meteor.Collection("locations");
+     
+           var mapOptions = {
+        streetViewControl: false,
+        scrollwheel: false,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+  map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
 });
 
@@ -61,6 +31,74 @@ Template.settings.events({
     
     }
 });
+
+
+  var makeMarker = function(loc) {
+    if(!loc.coordinates) return;
+
+    var pinColor = "AF83CC";
+    var pinImage = new google.maps.MarkerImage(
+      "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0,0),
+      new google.maps.Point(10, 34)
+    );
+
+    var marker = new google.maps.Marker({
+      map: map,
+      icon: pinImage,
+      position: new google.maps.LatLng(loc.coordinates[0], loc.coordinates[1])
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      window.open(loc.link);
+    });
+
+    // save to avoid duplicates later
+    locations[loc._id] = marker;
+  }
+
+    var createMap=function (latLng) {
+      var mapOptions = {
+        streetViewControl: false,
+        scrollwheel: false,
+        zoom: 15,
+        center: latLng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    };
+    
+    var addAutocomplete=function () {
+      var input = document.getElementById('searchTextField');
+      autocomplete = new google.maps.places.Autocomplete(input);
+      google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        var place = autocomplete.getPlace();
+        placeClickMarker(place.geometry.location);
+        map.setCenter(place.geometry.location);
+        map.setZoom(15);
+      });
+    }
+    var placeNavMarker=function (latLng) {
+      var image = "http://gmaps-samples.googlecode.com/svn/trunk/markers/blue/blank.png";
+      // this map is not always there>>>?
+      var blueIcon = new google.maps.Marker({
+          position: latLng,
+          map: map,
+          icon: image
+      });
+    }
+
+
+  function errorFunction(success) {
+    // alert("You've disabled your geolocation... So here are some pretty pictures of the Golden Gate bridge... You can always click around on the map or use the search to see more photos");
+    var latlng = new google.maps.LatLng(37.808631, -122.474470);
+    createMap(latlng);
+    placeNavMarker(navLatLng);
+    addAutocomplete();
+  }
+
+
 
 Template.content.rendered = function(){
 
@@ -103,6 +141,7 @@ Deps.autorun(function (c) {
 
             clinics.insert(record);
             placeNavMarker(results.geometry.location);
+            console.log(map);
             createMap(results.geometry.location);
 
             // this just doesn't want to place...
@@ -136,81 +175,18 @@ Deps.autorun(function (c) {
         
         
    }else{
+   /*
+    Move this code to a template function!!!
+   */
     // check to see if any new markers exist and put them on the screen somehow? without erasing everything...
-      var c = clinics.find({},{});
-      
-      
-      console.log(typeof c);
-      console.log(Object.keys(c));
-      console.log(c.collection.docs);
-      // gotta be a better way
-      var markers = []
-      for(key in c.collection.docs){
-        var val = c.collection.docs[key];
-        console.log(val);
-        markers.push[[val.x,val.y]];
-        
-      }
-      console.log('markers to show');
-      console.log(markers);
-      
-           var mapOptions = {
-        streetViewControl: false,
-        scrollwheel: false,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+          //map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
       // add different markers now?
       
-//      placeNavMarker(results.geometry.location);
-    // we're missing some fields.. ask for them.
+        //placeNavMarker(results.geometry.location);
    }
-   
-   
   });
   var isMobile = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|iemobile|BlackBerry)/);
-  console.log(isMobile);
-
-
-
-    var createMap=function (latLng) {
-      var mapOptions = {
-        streetViewControl: false,
-        scrollwheel: false,
-        zoom: 15,
-        center: latLng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    };
-    
-    
-    
-
-    
-    
-    var addAutocomplete=function () {
-      var input = document.getElementById('searchTextField');
-      autocomplete = new google.maps.places.Autocomplete(input);
-      google.maps.event.addListener(autocomplete, 'place_changed', function() {
-        var place = autocomplete.getPlace();
-        placeClickMarker(place.geometry.location);
-        map.setCenter(place.geometry.location);
-        map.setZoom(15);
-      });
-    }
-    var placeNavMarker=function (latLng) {
-      var image = "http://gmaps-samples.googlecode.com/svn/trunk/markers/blue/blank.png";
-      var blueIcon = new google.maps.Marker({
-          position: latLng,
-          map: map,
-          icon: image
-      });
-    }
-    
-    
-  if (navigator.geolocation) {
+      if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
   }
   else {
@@ -221,79 +197,30 @@ Deps.autorun(function (c) {
       var navLatLng = newLatLng(success);
       // annoying...
       createMap(navLatLng);
-
       placeNavMarker(navLatLng);
-      
-        
+      // weeirrrddd bug
+      // https://github.com/meteor/meteor/issues/321
+      // added fetch to make it work
+        var c = clinics.find({}, {fields: {_id: 1}}).fetch();
+        c.filter(function (arr){
+        // make new nat lav
+            var co = new google.maps.LatLng(arr.x, arr.y);
+            placeNavMarker(co);
+
+        });
+
       }
- 
+
+ // var map;
+ // var locations = {};
 
 
-
- //var geocoder;
-  var map;
-  var locations = {};
-
-  var makeMarker = function(loc) {
-    if(!loc.coordinates) return;
-
-    var pinColor = "AF83CC";
-    var pinImage = new google.maps.MarkerImage(
-      "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
-      new google.maps.Size(21, 34),
-      new google.maps.Point(0,0),
-      new google.maps.Point(10, 34)
-    );
-
-    var marker = new google.maps.Marker({
-      map: map,
-      icon: pinImage,
-      position: new google.maps.LatLng(loc.coordinates[0], loc.coordinates[1])
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-      window.open(loc.link);
-    });
-
-    // save to avoid duplicates later
-    locations[loc._id] = marker;
-  }
-
-  var geocodeAddress = function(loc) {
-    if (!loc.raw_address) return;
-
-    geocoder.geocode( { 'address': loc.raw_address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK && results[0] && results[0].geometry) {
-        var coordinates = [results[0].geometry.location["$a"], results[0].geometry.location["ab"]]
-        Locations.update(loc._id,
-          {
-            $set: {
-              coordinates: coordinates,
-              formatted_address: results[0].formatted_address
-            }
-          },
-          function(error) {
-            if(!error) {
-              makeMarker(loc)
-            }
-          }
-        );
-      }
-    });
-  };
-
-  function errorFunction(success) {
-    // alert("You've disabled your geolocation... So here are some pretty pictures of the Golden Gate bridge... You can always click around on the map or use the search to see more photos");
-    var latlng = new google.maps.LatLng(37.808631, -122.474470);
-    createMap(latlng);
-    placeNavMarker(navLatLng);
-    addAutocomplete();
-  }
     $('div#social').hide();
     $('div#clinic_add').hide();
     $('div#user_settings').hide();
 
 }
+
 
 //GOOGLE MAPS HELPERS
 
