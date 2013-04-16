6f5101ae89cfd0973d8b0f82813774a2eb177b5a
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2013-04-16 10:13:50 -0700

    add future methods for geocoded lookups

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 if (Meteor.isServer) {
   self= this;
-  clinics = new Meteor.Collection("clinics");
-  clinics._ensureIndex({ loc : "2d" });
+
+  markers = new Meteor.Collection("clinics");
+
+  markers._ensureIndex({ loc : "2d" });
+
+  Org = new Meteor.Collection('Org');
 
   users = new Meteor.Collection("user");
+
   Meteor.startup(function () {
-    
+    // theres no org..
     if (Org.find().count() === 0) {
       
       var names = ["Jack Chi",
@@ -25,13 +30,47 @@ if (Meteor.isServer) {
  });
  
    Meteor.methods({
+   
+    isLoggedIn : function(uid){
+    
+    },
+   
+    isAdmin : function(uid){
+        // do we have session here?
+        console.log(Session);
+        return true;
+    },
+    
+   
     findMarker : function(x,y){
         console.log(x);
         console.log(y);
+         markers._ensureIndex({ loc : "2d" });
          var theCenter = [[x,y],.1];
          // returns everything
-         var r = clinics.find({ loc : { "$within" : { "$center" : theCenter }}}).fetch();
+         var r = markers.find({ loc : { "$geoWithin" : { "$center" : theCenter }}}).fetch();
          console.log(r.length);
+        return r;
+    },
+    findmarkers : function(){
+        console.log('finding markers');
+        var c = markers.find({},{}).fetch();
+        c.filter(function(arr){
+            console.log(arr);
+        });
+        return markers.find({},{}).fetch();
+    },
+    markersIndex : function(){
+        console.log('finding markers index');
+        var c = markers.find({},{}).fetch(),r=[];
+        c.filter(function(arr){
+            console.log(arr);
+            var x = {}
+            // use underscore for this..
+            x._id = arr._id;
+            x.marker_name = arr.marker_name;
+            r.push(x);
+        });
         return r;
     }
   });
